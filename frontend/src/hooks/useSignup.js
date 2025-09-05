96ab1b4ae5fc05471da5598acee33100c7fab336
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-05 11:36:48 +0530

    fix: some issues

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -4,32 +4,31 @@ import { useAuthContext } from './useAuthContext';
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
-  const dispatch = useAuthContext();
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password) => {
     setError(null);
     setIsLoading(true);
 
-    const response = await fetch('api/user/signup', {
+    const response = await fetch('/api/user/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
     });
-
-    const json = response.json();
+    const json = await response.json();
 
     if (!response.ok) {
       setIsLoading(false);
       setError(json.error);
     }
-
     if (response.ok) {
-      // save user in local storage
+      // save the user to local storage
       localStorage.setItem('user', JSON.stringify(json));
 
-      // update auth context
-      dispatch({ type: 'LOGIN', payLoad: json });
+      // update the auth context
+      dispatch({ type: 'LOGIN', payload: json });
 
+      // update loading state
       setIsLoading(false);
     }
   };
